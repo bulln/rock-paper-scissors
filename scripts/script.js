fc06ab9f61498d18e2b562c0af3325d74a646c17
Nathan <nathan.richard.bull@gmail.com>
2021-08-16 20:19:56 -0400

    Add buttons and dynamic test displays

--- scripts/script.js
+++ scripts/script.js
@@ -1,82 +1,102 @@
+let round = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-game();
+const startButton = document.querySelector('#start-button');
+
+const playerScoreDiv = document.querySelector('#player-score');
+const computerScoreDiv = document.querySelector('#computer-score');
+
+const messageBlock = document.querySelector('#message-block');
+
+startButton.addEventListener('click', () => {
+    toggleButtons();
+});
+
+
+//create variables for rock, paper, scissor buttons to appear
+const selectionButtonDiv = document.querySelector('#selection-buttons');
+
+
+//toggle start button
+function toggleButtons(){
+    const startButton = document.getElementById('start-button');
+    if (startButton.style.display ==="none"){
+        startButton.style.display = "flex";
+        selectionButtonDiv.style.display = "none";
+    } else {
+        startButton.style.display = "none";
+        selectionButtonDiv.style.display = "flex";
+    }
+}
+
 
 function computerPlay(){
-    const computerOptions = ["ROCK", "PAPER", "SCISSORS"];
+    const computerOptions = ["rock", "paper", "scissors"];
     //randomly select a choice between, rock, paper or scissors and assign to computerSelecton
     let computerSelectionNum = Math.floor(Math.random() * computerOptions.length);
-    return computerOptions[computerSelectionNum].toUpperCase();
+    return computerOptions[computerSelectionNum];
 }
 
-function game(){
-    let playerScore;
-    let computerScore;
-    //create a string variable playerSelection for player choice
-    let playerSelection;
-    //creare a string variable computerSelecion for computer choice
-    let computerSelection;
-for (i = 0; i < 5;){
-    playerSelection = ValidatePlayerSelection();
-    computerSelection = computerPlay();
-    roundWinner = playRound(playerSelection, computerSelection);
-    switch(roundWinner){
-        case 'It\'s a draw':
-            alert(roundWinner + ', nobody wins this one!');
-            break;
-        case 'You win':
-            alert(roundWinner + ' round ' + (i+1));
-            i++;
-            playerScore += 1;
-            break;
-        case 'Computer wins':
-            alert(roundWinner + ' round ' + (i+1));
-            i++;
-            computerScore += 1;
-    }
-}
+function playRound() {
+    let computerSelection = computerPlay();
+    let playerSelection = this.id;
+    let winner;
 
-if (playerScore > computerScore){
-    alert('Congratulations! You win the game!');
-} else{
-    alert('Too bad... the computer has won this game.');
-}
-
-
-}
-
-//evaluate both playerSelection and computerSelection and determine outcome based on following criteria:
-function playRound(playerSelection, computerSelection){
-    //create a string variable called 'winner'
-   let winner;
-   //if both select the same choice, it's a draw
     if(playerSelection == computerSelection){
         winner = 'It\'s a draw';
-    //if playerSelection is rock: if computerSelection is paper - computer wins, if computerSelection is scissors, player wins
-   } else if(playerSelection === 'ROCK') {
-        computerSelection === 'PAPER' ? winner = 'Computer wins' : winner = 'You win';
-    //if playerSelection is scissors: if computerSelection is paper - player wins, if computerSelection is rock, computer wins
-   } else if(playerSelection === 'SCISSORS'){
-       computerSelection === 'PAPER' ? winner = 'You win' : winner = 'Computer wins';
-   } else{
-    //if playerSelection is paper: if computerSelection is rock - player wins, if computerSelection is scissors, computer wins
-       computerSelection === 'ROCK' ? winner = 'You win' : winner = 'Computer wins';
-   }
-   return winner;
-}
-
-function ValidatePlayerSelection(){
-    const playerOptions = ["ROCK", "PAPER", "SCISSORS"];
-    //prompt user to choose between rock, paper or scissors
-    let playerSelection = prompt('Rock, Paper, or Scissors?','').toUpperCase();
-    while (playerOptions.includes(playerSelection) == false){
-        alert('You did not enter a valid choice! Please try again')
-        //prompt user to choose between rock, paper or scissors
-        playerSelection = prompt('Rock, Paper, or Scissors?','').toUpperCase();
+    } else if(playerSelection === 'rock') {
+        computerSelection === 'paper' ? winner = 'Computer wins' : winner = 'You win';
+    } else if(playerSelection === 'scissors'){
+        computerSelection === 'paper' ? winner = 'You win' : winner = 'Computer wins';
+    } else{
+        computerSelection === 'rock' ? winner = 'You win' : winner = 'Computer wins';
     }
-    return playerSelection;      
+
+    switch(winner){
+        case 'It\'s a draw':
+            messageBlock.innerText = winner + ', nobody wins this one!';
+            break;
+        case 'You win':
+            messageBlock.innerText = winner + ' round ' + (round+1);
+            round++;
+            playerScore += 1;
+            playerScoreDiv.innerText = playerScore;
+            break;
+        case 'Computer wins':
+            messageBlock.innerText = winner + ' round ' + (round+1);
+            round++;
+            computerScore += 1;
+            computerScoreDiv.innerText = computerScore;
+    }
+    
+    if(round==5){
+        if (playerScore > computerScore){
+            messageBlock.innerText = 'Congratulations! You win the game!';
+        } else{
+            messageBlock.innerText = 'Too bad... the computer has won this game.';
+        }
+        toggleButtons();
+        round = 0;
+        playerScore = 0;
+        computerScore = 0;
+        playerScoreDiv.innerText = playerScore;
+        computerScoreDiv.innerText = computerScore;
+        startButton.children[0].innerText = 'Play Again';
+    } else{
+        return;
+    }
+
+
 }
 
+const selectionButtons = document.querySelectorAll('.selection-button');
 
-//if player wins: assign 'winner' the string 'You win!', if computer wins: assign 'winner' the string 'You lose!', if it's a draw assign 'winner' the string 'It's a draw!'
-// display "The computer chose 'computerSelection', "'winner' string in browser
+selectionButtons.forEach(selectionButton => selectionButton.addEventListener('click', playRound));
+
+
+
+
+
+
 
